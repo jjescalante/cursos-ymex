b28e378034b33e729dd12d7b083dafdf3046d4c4
jjescalante <jjet@live.com.mx>
2019-02-28 20:53:07 +0100

    [REF] codetasty test

--- jjodoo/models/models.py
+++ jjodoo/models/models.py
@@ -10,7 +10,6 @@ from odoo import models, fields, api
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
-#
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
