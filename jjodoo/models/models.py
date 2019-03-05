8c2f415fbb06710320006a322f6c5767ecd3ecae
jjescalante <jjet@live.com.mx>
2019-03-05 11:17:34 +0100

    [ADD] basic fields

--- jjodoo/models/models.py
+++ jjodoo/models/models.py
@@ -2,20 +2,33 @@
 
 from odoo import models, fields, api
 
+
 class Query(models.Model):
     _name = 'jjodoo.query'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    roster_emp = fields.One2many('jjodoo.employees', 'employees_ids')
+    
+    
+class Courses(models.Model):
+    _name = 'jjodoo.courses'
 
-# class jjodoo(models.Model):
-#     _name = 'jjodoo.jjodoo'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    course_name = fields.Char(string="Course Name", required=True)
+    start_date = fields.Date(requiered=True)
+    end_date = fields.Date(requiered=True)
+    duration = fields.Integer()
+    instructor = fields.Char(string="Instructor´s Name", required=True)
+    company = fields.Char(string="Company", required=True)
+    code = fields.Char(string="Code", required=True)
+    
+    
+class Employees(models.Model):
+    _name = 'jjodoo.employees'
+    
+    roster = fields.Char(string="Roster", required=True)
+    employees_ids = fields.Many2one('jjodoo.query', ondelete='cascade', required=True)
+    name = fields.Char(string="Name", required=True)
+    date_admission = fields.Date()
+    position = fields.Char(string="Position", required=True)
+    area = fields.Char(string="Area", required=True)
